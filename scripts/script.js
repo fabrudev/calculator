12ed290f21f03d479f54a9b8e1cb75f69feb5453
FabruDJ <fabrizarateok@gmail.com>
2022-07-28 22:23:35 -0300

    fixes 2.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,33 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.btn');
 
-let value = '0';
+let value = '';
 
-function escuchando (event) {
-    value += event.target.textContent;
-    display.textContent = value;
+function calcular (event) {
+    const tecla = event.target.textContent;
+
+    switch (tecla) {
+        case "=":
+            display.textContent = eval(value.replace('x', '*'));
+            value = eval(value.replace("x", "*"));
+            break;
+        case "C": 
+            display.textContent = "0";
+            value = "";
+            break;
+        case "+":
+        case "-":
+        case "x":
+        case "/":
+        default:
+            value+= tecla;
+            display.textContent = value;
+    }
 }
 
 
 for(let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', escuchando);
+    buttons[i].addEventListener('click', calcular);
 }
 
 display.textContent = value;
